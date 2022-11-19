0fc2afac9a4d309cd470fda0d244408665926111
konti <kontos.balazs.94@gmail.com>
2022-11-19 17:20:04 +0100

    select feltöltése dinamikus adatokkal

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-// JS
+const cities = ['Budapest', 'Győr', 'Debrecen', 'Miskolc', 'Pécs', 'Sopron', 'Szeged', 'Gyékényes'];
+
+renderSelect();
+
+function renderSelect() {
+    let selectValues = document.getElementById('cities');
+
+    for (let city in cities) {
+        selectValues.innerHTML += `
+            <option value="${cities[city]}">${cities[city]}</option>
+        `;
+    }
+}
